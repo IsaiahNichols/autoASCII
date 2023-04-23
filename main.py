cb4b10237b5cde7f36a3f5b79fb0c84153260e34
Isaiah Nichols <isaiah.nichols180@gmail.com>
2023-04-22 22:16:33 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,37 @@
+from PIL import Image
+
+shades = {
+    0.4 : "-",
+    0.5 : "~",
+    0.6 : "/",
+    0.7 : "+",
+    0.8 : "#",
+    0.9 : "@",
+    1: "&"
+}
+
+image = Image.open("assets/image.png")
+px = image.load()
+
+skip = 25
+
+for i in range(image.size[1]):
+    if i % skip == 0:
+        for j in range(image.size[0]):
+            if j % skip == 0:
+                val = 0
+                for k in range(len(px[i, j])):
+                    val += (px[i, j][k])/255
+                
+                val /= len(px[i, j])
+                val = 1-val
+                val = round(val, 1)
+
+                if val < 0.4:
+                    val = 0.4
+                elif val > 1:
+                    val = 1
+
+                print(shades[val], end="")
+    
+        print()
